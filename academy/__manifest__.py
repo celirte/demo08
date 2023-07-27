9efa387e67a8b6d8040514a29dc303a3a1b43e9b
celirte <johandavid27@hotmail.com>
2023-07-27 22:17:47 +0000

    [ADD] New Module

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Module to handle Courses and Sessions""",
+    'description':"""Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license':'OPL-1',
+    'author': 'johan',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
