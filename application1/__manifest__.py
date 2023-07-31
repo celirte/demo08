5a5e988f52aaf8ae7f3c8384a3200b5f8f393d10
celirte <johandavid27@hotmail.com>
2023-07-31 22:44:55 +0000

    [ADD] New Application1 module

---
+++ application1/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Application 1',
+    'summary': """Module to handle custom development JOHAN""",
+    'description':"""""",
+    'license':'OPL-1',
+    'author': 'johan',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
